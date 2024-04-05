f6226fb9e0dcb09c200156f038b03cc3c15129ec
DevSouza05 <peterson.moraes2005@gmail.com>
2024-04-05 11:16:31 -0300

    update in information of the pizza-ite

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-const c = (el)=>document.querySelector(el);
-const cs= (el)=>document.querySelectorAll(el);
+const c = (el) => document.querySelector(el);
+const cs = (el) => document.querySelectorAll(el);
 
 // mapeando pizzaJson
-pizzaJson.map((item , index)=>{
+pizzaJson.map((item, index) => {
     let pizzaItem = c(".models .pizza-item").cloneNode(true);
 
     //chave da pizza especifica
@@ -16,23 +16,22 @@ pizzaJson.map((item , index)=>{
     //fim de peenchimento de informaçoes em pizzaItem
 
     // open modal
-    pizzaItem.querySelector("a").addEventListener("click", (e)=>{
-        e.preventDefault();
+    pizzaItem.querySelector("a").addEventListener("click", (e) => {
+    e.preventDefault();
 
         //busca elemento mais perto
         let key = e.target.closest(".pizza-item").getAttribute("data-key");
-        // console.log(key)
-        // console.log(pizzaJson[key])
-        c(".pizzaInfo h1").innerHTML = pizzaJson [key].name
-        c(".pizzaInfo--desc").innerHTML = pizzaJson [key].description
 
+        c(".pizzaBig img").src = pizzaJson[key].img
+        c(".pizzaInfo h1").innerHTML = pizzaJson[key].name
+        c(".pizzaInfo--desc").innerHTML = pizzaJson[key].description
+        c(".pizzaInfo--size").innerHTML = pizzaJson [key].sizes
+        c(".pizzaInfo--actualPrice").innerHTML =`R$ ${pizzaJson[key].price.toFixed(2)}`;
 
-
-       
-        c(".pizzaWindowArea").style.opacity= 0;
-        c(".pizzaWindowArea").style.display="flex";
-        setTimeout(()=>{
-            c(".pizzaWindowArea").style.opacity= 1;
+        c(".pizzaWindowArea").style.opacity = 0;
+        c(".pizzaWindowArea").style.display = "flex";
+        setTimeout(() => {
+            c(".pizzaWindowArea").style.opacity = 1;
         }, 200);
     })
 
